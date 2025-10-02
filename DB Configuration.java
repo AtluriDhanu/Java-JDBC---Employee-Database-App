f2fe1764b2c4de799a7aad3f3681c85bd8a7f951
Atluri Dhanush <atluridhanu129@gmail.com>
2025-10-02 13:58:12 +0530

    Database Configuration File

---
+++ DB Configuration.java
@@ -0,0 +1,8 @@
+package employee;
+
+public class DBConfig {
+
+	public static final String url = "jdbc:mysql://localhost:3306/company";
+	public static final String username = "root";
+	public static final String password = "1234"; 
+}
